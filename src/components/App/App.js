1b7ed29e2c9b4468e753211fb25bd9c69978cc4c
OmarSidds <101479068+OmarSidds@users.noreply.github.com>
2024-01-17 16:02:48 -0500

    Hard-coded array of track objects, added event handler code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,41 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import React, { useState } from 'react';
+
 
 function App() {
-/*
-  const containerStyle = {
-    display: 'flex', 
-    justifyContent: 'center',  
-    alignItems: 'center',
+
+  const arrSearch = [
+    {
+      name: 'Hard to explain',
+      artist: 'The Strokes',
+      album: 'Is this it',
+      id: 1
+    },
+    {
+      name: 'Still into You',
+      artist: 'Paramore',
+      album: 'Paramore',
+      id: 2
+    },
+    {
+      name: 'Chocolate',
+      artist: 'The 1975',
+      album: 'The 1975',
+      id: 3
+    }
+  ];
+
+  const [search_res, setSearch_res] = useState(null);
+
+  const handleChange = (event) => {
+    // Update the state with the new input value
+    setSearch_res();
   };
-*/
+
+
+
   return (
     <div>
       <nav className="navBar">
@@ -25,6 +51,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,13 @@
-import './SearchResults.css'
+import './SearchResults.css';
+import React, { useState, useCallback} from "react";
 
 function SearchResults(){
-    const items = ['Item 1', 'Item 2', 'Item 3','Item 1', 'Item 2', 'Item 3','Item 1', 'Item 2', 'Item 3','Item 1', 'Item 2', 'Item 3'];
+    
 
 
     return(
         <div className='SearchResults'>
             <h2>Results</h2>
-            <ul>
-                {items.map((item, index) => (
-                <li key={index}>{item}</li>
-                ))}
-            </ul>
         </div>
     )
 }
